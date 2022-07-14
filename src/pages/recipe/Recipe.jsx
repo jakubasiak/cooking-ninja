8caddf2e328486fdf7dc37ff9ffc9a8ec9667c69
Jakub Kubasiak <xjk@pfa.dk>
2022-07-14 12:11:44 +0200

    add create recipe form

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,7 +1,26 @@
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
 import './Recipe.css'
 
 export default function Recipe() {
+  const { id } = useParams()
+  const {data: recipe, isPending, error} = useFetch(`http://localhost:3000/recipes/${id}`)
   return (
-    <div>Recipe</div>
+    <div className='recipe'>
+      {error && <p className='error'>{ error }</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {recipe && (
+        <>
+          <h2 className='page-title'>{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime}</p>
+          <ul>
+            {recipe.ingredients.map(ingredient => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
+          </ul>
+          <p className='method'>{recipe.method}</p>
+        </>
+      )}
+    </div>
   )
 }
--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,7 +1,22 @@
+import { useLocation } from 'react-router-dom'
+import RecipeList from '../../components/RecipeList'
+import { useFetch } from '../../hooks/useFetch'
 import './Search.css'
 
 export default function Search() {
+  const queryString = useLocation().search
+  const queryParams = new URLSearchParams(queryString)
+  const query = queryParams.get('q')
+
+  const url = `http://localhost:3000/recipes?q=${query}`
+  const {data: recipes, isPending, error} = useFetch(url)
+
   return (
-    <div>Search</div>
+    <div>
+      <h2 className='page-title'>Recipes including "{query}"</h2>
+    {error && <p className='error'>{ error }</p>}
+    {isPending && <p className='loading'>Loading...</p>}
+    {recipes && <RecipeList recipes={ recipes } />}
+  </div>
   )
 }
